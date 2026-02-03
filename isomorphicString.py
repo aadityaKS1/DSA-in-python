f8181dc42930a40acdaac1272ebf5917f4e5caca
Aaditya Kumar Singh <aaditya.creates@gmail.com>
2026-02-03 23:18:55 +0545

    dsa8

---
+++ isomorphicString.py
@@ -0,0 +1,15 @@
+
+def isIsomorphic( s: str, t: str) -> bool:
+    m1,m2=[0]*256,[0]*256
+    n=len(s)
+    for i in range(n):
+        if  m1[ord(s[i])]!=m2[ord(t[i])]:
+            return False
+        m1[ord(s[i])]=i+1
+        m2[ord(t[i])]=i+1
+
+    return True
+
+s = "egg" 
+t = "add"
+print(isIsomorphic(s,t))
