98862a4aa54d8eee45e5a214d62b4512c92ccca4
Aaditya Kumar Singh <aaditya.creates@gmail.com>
2026-02-14 09:00:49 +0545

    anagramscheck

---
+++ ànagrams.py
@@ -0,0 +1,25 @@
+# def checkAnagram(s:str,t:str)->bool:
+#     if len(s)!=len(t):
+#         return False
+#     if sorted(s)==sorted(t):
+#         return True
+#     return False
+        
+def checkAnagram(s:str,t:str)->bool:
+    if len(s)!=len(t):
+        return False
+    m=[0]*26
+    for char in s:
+        m[ord(char)-ord('A')]+=1
+    for char in t:
+        m[ord(char)-ord('A')]-=1
+    for count in m:
+        if count!=0:
+            return False
+    return True   
+        
+        
+        
+s="ABCDEF"
+t="BCEFMD"
+print(checkAnagram(s,t))     
